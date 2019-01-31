0590283edc8e20ed7b4dee2af735a01759a78db3
ItoMasaki <l1sum@icloud.com>
2019-01-31 17:10:49 +0900

    ros2で必要

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='controle_turtlebot3',
+    version='0.0.0',
+    packages=[],
+    py_modules=[],
+    install_requires=['setuptools'],
+    author='Ito Masaki',
+    author_email='is0449sh@ed.ritsumei.ac.jp',
+    maintainer='Ito Masaki',
+    maintainer_email='is0449sh@ed.ritsumei.ac.jp',
+    keywords=['ROS2'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python3',
+        'Topic :: Software Development',
+    ],
+    description='controle turtlebot3.',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    zip_safe=True,
+)
