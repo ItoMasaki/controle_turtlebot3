4e4f2fe07e83704ff5ca660c4ac9ce3bbf64246a
ItoMasaki <l1sum@icloud.com>
2019-02-12 15:58:40 +0900

    今後使わない可能性が大きい

--- src/controle.py
+++ src/controle.py
@@ -2,6 +2,9 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
+from time import sleep
+
+from sys import exit
 
 class Controle(Node):
     def __init__(self):
@@ -11,7 +14,28 @@ class Controle(Node):
 
         self.twist = Twist()
 
-        self.run(0.01, 0.05)
+        for i in range(2):
+            self.run(0.20, 0.0)
+            sleep(19)
+            self.run(0.0, 0.0)
+            sleep(1)
+
+            self.run(0.0, 0.25)
+            sleep(6.5)
+            self.run(0.0, 0.0)
+            sleep(1)
+
+            self.run(0.20, 0.0)
+            sleep(4)
+            self.run(0.0, 0.0)
+            sleep(1)
+
+            self.run(0.0, 0.25)
+            sleep(5.8)
+            self.run(0.0, 0.0)
+            sleep(1)
+
+        self.run(0.0, 0.0)
 
     def run(self, l, a):
         self.twist.linear.x = float(l)
@@ -25,9 +49,10 @@ def main():
     node = Controle()
 
     
-    rclpy.spin(node)
-
-    node.destroy_node()
+    try:
+        rclpy.spin(node)
+    except:
+        node.destroy_node()
 
     rclpy.shutdown()
 
